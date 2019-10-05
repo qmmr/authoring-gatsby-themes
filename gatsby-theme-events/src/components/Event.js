59eda40ec7b423b909ec7a274e5f7c55e56a3989
Marcin Kumorek <kumeek@gmail.com>
2019-10-05 14:01:24 +0200

    Refactor event dates :see_no_evil:

--- gatsby-theme-events/src/components/Event.js
+++ gatsby-theme-events/src/components/Event.js
@@ -1,12 +1,37 @@
 import React from 'react'
 
+// TODO: Change locale date string to Europe?
+const getDate = (date, { day = true, month = true, year = true } = {}) =>
+  date.toLocaleDateString('en-US', {
+    day: day ? 'numeric' : undefined,
+    month: month ? 'long' : undefined,
+    year: year ? 'numeric' : undefined
+  })
+
+const EventDate = ({ startDate, endDate }) => {
+  const start = new Date(startDate)
+  const end = new Date(endDate)
+  const isOneDay = start.toDateString() === end.toDateString()
+
+  return (
+    <>
+      <time dateTime={start.toISOString()}>{getDate(start, { year: isOneDay })}</time>
+      {!isOneDay && (
+        <>
+          -<time dateTime={end.toISOString()}>{getDate(end, { month: start.getMonth() !== end.getMonth() })}</time>
+        </>
+      )}
+    </>
+  )
+}
+
 const EventTemplate = ({ name, url, startDate, endDate, location, slug }) => (
   <>
     <h1>
       {name} ({location})
     </h1>
     <p>
-      {startDate} - {endDate}
+      <EventDate startDate={startDate} endDate={endDate} />
     </p>
     <p>
       Website: <a href={url}>{url}</a>
